20f48823071d11b9f967ed521ab6f13494afaaad
asiripr <asiripramodaya2000@gmail.com>
2024-09-09 21:17:29 +0530

    fixed some issues

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,14 +12,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Gender? selectedGender;
   int height = 0;
-  int weight = 0;
+  int weight = 1;
+  // double bmi = height.toDouble()/(weight.toDouble()*weight.toDouble());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("BMI Calculator")),
       body: Expanded(
         child: Column(
-          children: <Widget>[
+          children: [
             const Text("Height", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w900)),
             SizedBox(
               child: Slider(
@@ -35,12 +36,33 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
 
             Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Text(height.toString(), style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
                 const Text("cm", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               ],
             ),
+            const SizedBox(height: 20,),
+            const Text("Weight", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w900)),
+            SizedBox(
+              child: Slider(
+                value: weight.toDouble(),
+                min: 0,
+                max: 100,
+                onChanged: (double newValue) {
+                  setState(() {
+                    weight = newValue.round();
+                  });
+                },
+              ),
+            ),
+
+            Row(
+              children: [
+                Text(weight.toString(), style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                const Text("kg", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ],
+            ),
+
           ],
         ),
       ),
